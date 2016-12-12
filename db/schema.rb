bb43974ad09c7021b07ea0b86697a01ade99f04e
Ganbold <ganbold.li@gmail.com>
2016-12-11 18:06:49 -0800

    devise done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110214948) do
+ActiveRecord::Schema.define(version: 20161212020223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
