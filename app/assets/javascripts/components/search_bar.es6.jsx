fca8dfe0cdae47043db7eb0da74df2d937be9d1f
Ganbold <ganbold.li@gmail.com>
2017-01-04 18:08:02 -0800

    working

--- app/assets/javascripts/components/search_bar.es6.jsx
+++ app/assets/javascripts/components/search_bar.es6.jsx
@@ -1,14 +1,24 @@
 class SearchBar extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { value: '' };
+  }
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    console.log("search");
+  }
   render () {
-      return (
-        <div className="input-group">
-           <input type="text" className="form-control" placeholder="Search a word" />
-           <span className="input-group-btn">
-               <button className="btn btn-primary" type="button">
-                   <span className="glyphicon glyphicon-search"></span>
-               </button>
-           </span>
-        </div>
-      );
-    }
+    return (
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Search a word" onChange={event => this.handleChange(event)} value={this.state.value} />
+        <span className="input-group-btn">
+         <button className="btn btn-primary" type="button" onClick={event => this.handleSubmit(event)}>
+           <span className="glyphicon glyphicon-search"></span>
+         </button>
+        </span>
+      </div>
+    );
+  }
 }
