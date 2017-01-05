c212b6f4593335b48ec7b11204db3fe9fecc09c8
Ganbold <ganbold.li@gmail.com>
2017-01-05 10:48:49 -0800

    remove comments

--- app/controllers/eng_word_translations_controller.rb
+++ app/controllers/eng_word_translations_controller.rb
@@ -1,28 +1,20 @@
 class EngWordTranslationsController < ApplicationController
   before_action :set_eng_word_translation, only: [:show, :edit, :update, :destroy]
 
-  # GET /eng_word_translations
-  # GET /eng_word_translations.json
   def index
     @eng_word_translations = EngWordTranslation.all
   end
 
-  # GET /eng_word_translations/1
-  # GET /eng_word_translations/1.json
   def show
   end
 
-  # GET /eng_word_translations/new
   def new
     @eng_word_translation = EngWordTranslation.new
   end
 
-  # GET /eng_word_translations/1/edit
   def edit
   end
 
-  # POST /eng_word_translations
-  # POST /eng_word_translations.json
   def create
     @eng_word_translation = EngWordTranslation.new(eng_word_translation_params)
 
@@ -37,8 +29,6 @@ class EngWordTranslationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /eng_word_translations/1
-  # PATCH/PUT /eng_word_translations/1.json
   def update
     respond_to do |format|
       if @eng_word_translation.update(eng_word_translation_params)
@@ -51,8 +41,6 @@ class EngWordTranslationsController < ApplicationController
     end
   end
 
-  # DELETE /eng_word_translations/1
-  # DELETE /eng_word_translations/1.json
   def destroy
     @eng_word_translation.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class EngWordTranslationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_eng_word_translation
       @eng_word_translation = EngWordTranslation.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def eng_word_translation_params
       params.fetch(:eng_word_translation, {})
     end
