e16f0db4935903c4f14d3f8e8c9b3a3abf90700a
Ganbold <ganbold.li@gmail.com>
2017-01-04 17:44:01 -0800

    add searchbar ui

--- app/assets/javascripts/components/search_bar.es6.jsx
+++ app/assets/javascripts/components/search_bar.es6.jsx
@@ -1,6 +1,14 @@
 class SearchBar extends React.Component {
   render () {
-    return <div />;
-  }
+      return (
+        <div className="input-group">
+           <input type="text" className="form-control" placeholder="Search a word" />
+           <span className="input-group-btn">
+               <button className="btn btn-primary" type="button">
+                   <span className="glyphicon glyphicon-search"></span>
+               </button>
+           </span>
+        </div>
+      );
+    }
 }
-
