6f111fb641534d925de3bce560b9b4f56768fffc
Ganbold <ganbold.li@gmail.com>
2017-01-05 20:20:00 -0800

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :eng_words
-  resources :eng_word_translations
+  resources :eng_words do
+    resources :eng_word_translations do
+
+    end
+  end
 
   get 'welcome_page/welcome'
   root 'welcome_page#welcome'
