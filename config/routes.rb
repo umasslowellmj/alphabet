9139e6cc18796ff05b0f6cabbbdf23d4dde6db27
Ganbold <ganbold.li@gmail.com>
2017-01-07 20:03:10 -0800

    add search routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     get "/new/eng_words/:eng_word_id", :to => "eng_word_translations#new", :as => :new, :on => :collection
   end
 
-  resources :eng_words
+  resources :eng_words do
+    get '/search', :to => 'eng_words#search', :on => collection
+  end
 
 
   get 'welcome_page/welcome'
