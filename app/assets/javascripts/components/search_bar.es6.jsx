86319902ba83e26a4a2f1097a7160cf53533fa73
Ganbold <ganbold.li@gmail.com>
2017-01-11 17:13:51 -0800

    fix search for enter key

--- app/assets/javascripts/components/search_bar.es6.jsx
+++ app/assets/javascripts/components/search_bar.es6.jsx
@@ -9,10 +9,15 @@ class SearchBar extends React.Component {
   handleSubmit(event) {
     window.location.href = window.location.origin + "/search/" + this.state.value
   }
+  handleKeyPress(event) {
+    if (event.charCode == 13) {
+      window.location.href = window.location.origin + "/search/" + this.state.value
+    }
+  }
   render () {
     return (
       <div className="input-group">
-        <input type="text" className="form-control" placeholder="Search a word" onChange={event => this.handleChange(event)} value={this.state.value} />
+        <input type="text" className="form-control" placeholder="Search a word" onChange={event => this.handleChange(event)} onKeyPress= {event => this.handleKeyPress(event)} value={this.state.value} />
         <span className="input-group-btn">
          <button className="btn btn-primary" type="button" onClick={event => this.handleSubmit(event)}>
            <span className="glyphicon glyphicon-search"></span>
